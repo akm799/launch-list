1449bf4031ea6fd9c2f95d99a2fdaa89ccfca247
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2020-01-16 16:08:41 +0000

    Added image loading progress bar.

--- LaunchListRx/app/src/main/java/co/uk/akm/test/launchlistrx/presentation/ui/list/LaunchViewHolder.kt
+++ LaunchListRx/app/src/main/java/co/uk/akm/test/launchlistrx/presentation/ui/list/LaunchViewHolder.kt
@@ -1,6 +1,7 @@
 package co.uk.akm.test.launchlistrx.presentation.ui.list
 
 import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -9,6 +10,10 @@ import co.uk.akm.test.launchlistrx.R
 import co.uk.akm.test.launchlistrx.domain.model.Launch
 import co.uk.akm.test.launchlistrx.util.date.reformatAsDate
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class LaunchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
@@ -28,10 +33,13 @@ class LaunchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     }
 
     private fun setMissionPatch(launch: Launch) {
+        val loadingListener = ImageLoadingListener(itemView.findViewById(R.id.missionPatchProgress))
+
         Glide.with(itemView)
             .load(launch.missionPatch)
             .error(R.drawable.ic_launcher_foreground)
             .fallback(R.drawable.ic_launcher_foreground)
+            .listener(loadingListener)
             .into(itemView.findViewById(R.id.missionPatch))
     }
 
@@ -66,5 +74,29 @@ class LaunchViewHolder(view: View) : RecyclerView.ViewHolder(view) {
                 visibility = View.INVISIBLE
             }
         }
+    }
+}
+
+private class ImageLoadingListener(private val progressView: View) : RequestListener<Drawable> {
+
+    override fun onLoadFailed(
+        e: GlideException?,
+        model: Any?,
+        target: Target<Drawable>?,
+        isFirstResource: Boolean
+    ): Boolean {
+        progressView.visibility = View.GONE
+        return false
+    }
+
+    override fun onResourceReady(
+        resource: Drawable?,
+        model: Any?,
+        target: Target<Drawable>?,
+        dataSource: DataSource?,
+        isFirstResource: Boolean
+    ): Boolean {
+        progressView.visibility = View.GONE
+        return false
     }
 }
